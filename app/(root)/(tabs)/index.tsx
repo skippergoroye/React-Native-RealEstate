d957bd3f655825ab193b6e2d254c75811dc1e79d
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-07-27 19:08:12 +0100

    feat: filter added  here

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,6 +10,7 @@ import images from "@/constants/images";
 import icons from "@/constants/icons";
 import Search from "@/components/Search";
 import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
 
 const index = () => {
   return (
@@ -43,7 +44,32 @@ const index = () => {
             </TouchableOpacity>
           </View>
 
-          <FeaturedCard />
+          <View className="flex flex-row gap-5 mt-5">
+            <FeaturedCard />
+            <FeaturedCard />
+            {/* <Card /> */}
+          </View>
+        </View>
+
+
+
+
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300 ">
+            Our Recommendation
+          </Text>
+          <TouchableOpacity>
+            <Text className="text-base  font-rubik-bold text-primary-300    ">
+              See all
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+
+        <Filters />
+
+        <View className="flex flex-row gap-5 mt-5">
+          <Card />
           <Card />
         </View>
       </View>
--- components/Cards.tsx
+++ components/Cards.tsx
@@ -50,10 +50,38 @@ export const FeaturedCard = ({ onPress }: Props) => {
   );
 };
 
-export const Card = () => {
+export const Card = ({onPress }: Props) => {
   return (
-    <View>
-      <Text>Card</Text>
-    </View>
+    <TouchableOpacity  className="flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative" onPress={onPress}>
+      <View className="flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
+        <Image source={icons.star} className="size-2.5" />
+        <Text className="text-xs font-rubik-bold text-primary-300 ml-0.5">
+          4.4
+        </Text>
+      </View>
+
+
+        <Image source={images.newYork} className="w-full h-40 rounded-lg" />
+
+      <View className="flex flex-col mt-2">
+        <Text
+          className="text-base font-rubik-bold text-black-300"
+         
+        >
+          Cozy Studio
+        </Text>
+        <Text className="text-xs  font-rubik text-black-200">
+          22 w 15th st, New York
+        </Text>
+ 
+
+
+        <View className="flex flex-row items-center justify-between mt-2">
+          <Text className="text-base font-rubik-bold text-primary-300">$2500</Text>
+          <Image source={icons.heart} className="w-5 h-5 size-5 mr-2" />
+
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
